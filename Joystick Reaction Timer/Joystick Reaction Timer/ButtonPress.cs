b79b60869282e1025fb39cc017e71747134c628e
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2023-02-05 01:02:26 -0500

    Upload

---
+++ Joystick Reaction Timer/Joystick Reaction Timer/ButtonPress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joystick_Reaction_Timer
+{
+    internal class ButtonPress
+    {
+        public DateTime time { get; set; }
+        public String buttonName { get; set; }
+
+        public ButtonPress(DateTime time, String buttonName)
+        {
+            this.time=time;
+            this.buttonName=buttonName;
+        }
+    }
+}
---
+++ Joystick Reaction Timer/Joystick Reaction Timer/Form1.cs
@@ -0,0 +1,290 @@
+using SharpDX.DirectInput;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+using System.Linq.Expressions;
+using System.Drawing;
+
+namespace Joystick_Reaction_Timer
+{
+    public partial class Form1 : Form
+    {
+        String connectController = "Please connect a controller...";
+        String ready = "Ready.";
+        bool joystickConnected = false;
+
+        Joystick joystick = null;
+
+
+        Thread backgroundThread;
+        Thread backgroundThread1;
+
+        State state = State.Init;
+
+        int buttonRequestCounts = 3;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundThread = new Thread(new ThreadStart(this.joystickHandler));
+            backgroundThread.IsBackground = true;
+            backgroundThread.Start();
+
+            backgroundThread1 = new Thread(new ThreadStart(this.programHandler));
+            backgroundThread1.IsBackground = true;
+            backgroundThread1.Start();
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            backgroundThread.Interrupt();
+            backgroundThread1.Interrupt();
+        }
+
+        private void joystickHandler()
+        {
+            try
+            {
+                // Initialize DirectInput
+                var directInput = new DirectInput();
+                // Find a Joystick Guid
+                var joystickGuid = Guid.Empty;
+
+                while (true)
+                {
+                    if (joystick == null)
+                    {
+                        foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+                            joystickGuid = deviceInstance.InstanceGuid;
+                        if (joystickGuid != Guid.Empty)
+                        {
+                            joystick = new Joystick(directInput, joystickGuid);
+                            // Set BufferSize in order to use buffered data.
+                            joystick.Properties.BufferSize = 128;
+                            // Acquire the joystick
+                            joystick.Acquire();
+                            joystickConnected = true;
+                        }
+                    }
+                    else
+                    {
+                        if (!directInput.IsDeviceAttached(joystickGuid))
+                        {
+                            joystickGuid = Guid.Empty;
+                            joystick = null;
+                            joystickConnected = false;
+                            state = State.Init;
+                        }
+                        else
+                        {
+                            joystick.Poll();
+                        }
+                    }
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+            }
+        }
+        Stopwatch sw;
+        Random randomButton = new Random();
+
+        List<ButtonPress> buttonRequests = new List<ButtonPress>();
+        List<ButtonPress> buttonPresses= new List<ButtonPress>();
+
+
+        private void programHandler()
+        {
+            try
+            {
+                while (true)
+                {
+                    switch (state)
+                    {
+                        case State.Init:
+                            if (joystickConnected)
+                            {
+                                this.label1.Invoke((MethodInvoker)delegate
+                                {
+                                    this.label1.Text = ready;
+                                    this.button1.Enabled = true;
+                                });
+                            }
+                            else
+                            {
+                                this.label1.Invoke((MethodInvoker)delegate
+                                {
+                                    this.label1.Text = connectController;
+                                    this.button1.Enabled = false;
+                                });
+                            }
+                            break;
+                        case State.step0:
+                            sw = Stopwatch.StartNew(); // Start a timer
+                            state = State.step1;
+                            break;
+                        case State.step1:
+                            int delay = 5000;
+                            
+                            if (sw.ElapsedMilliseconds>delay) // Allow time for the user to pickup the contoller after picking start.
+                            {                          
+                                state = State.step2;
+                                break;
+                            }
+                            this.label1.Invoke((MethodInvoker)delegate
+                            {
+                                this.label1.Text = String.Format("Starting in {0} seconds",  ((delay - sw.ElapsedMilliseconds) / 1000.0));
+                                this.button1.Enabled = false;
+                            });
+                            break;
+                        case State.step2:
+                            this.label1.Invoke((MethodInvoker)delegate
+                            {
+                                this.label1.Text = String.Format("Press Button: {0}", "--");
+                                this.button1.Enabled = false;
+                            });
+                            state = State.step3;
+                            sw = Stopwatch.StartNew(); // Start a timer
+
+                            break;
+                        case State.step3:
+                            if(sw.ElapsedMilliseconds>400)
+                            {
+                                state = State.step4;
+                            }
+                            break;
+                        case State.step4:
+                            int button = randomButton.Next(0, 9);
+                            String buttonName = Enum.GetName(typeof(LogitechF310Buttons), button).Replace("_"," ");
+                            this.label1.Invoke((MethodInvoker)delegate
+                            {
+                                this.label1.Text = String.Format("Press Button: {0}",buttonName);
+                                this.button1.Enabled = false;
+                            });
+                            state = State.step5;
+                            buttonRequests.Add(new ButtonPress(DateTime.Now, buttonName));
+                            sw = Stopwatch.StartNew(); // Start a timer
+
+                            break;
+
+                        case State.step5:
+                            var datas = joystick.GetBufferedData();
+                            foreach (var data in datas)
+                            {
+                                String offset = data.Offset.ToString();
+                                if (offset.Contains("Buttons") && data.Value==128)
+                                {
+                                    offset = offset.Replace("Buttons","");
+                                    String buttonName1 = Enum.GetName(typeof(LogitechF310Buttons), int.Parse(offset)).Replace("_", " ");
+                                    buttonPresses.Add(new ButtonPress(DateTime.Now, buttonName1));
+
+                                    if (buttonRequests.Last().buttonName.Equals(buttonName1))
+                                    {
+                                        if(buttonRequests.Count < buttonRequestCounts)
+                                        {
+                                            state = State.step2;
+                                        }
+                                        else
+                                        {
+                                            state = State.step6;
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                            break;
+                        case State.step6:
+                            this.label1.Invoke((MethodInvoker)delegate
+                            {
+                                this.label1.Text = "Complete";
+                                this.button1.Text = "Export";
+                                this.button1.Enabled = true;
+                            });
+                            state = State.step7;
+                            break;
+
+                        case State.step7:
+                            break;
+                    }
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text=="Start")
+                state = State.step0;
+            if(button1.Text == "Export")
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+                saveFileDialog.FilterIndex = 0;
+                saveFileDialog.RestoreDirectory = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = saveFileDialog.FileName;
+                    using (StreamWriter writer = new StreamWriter(fileName))
+                    {
+                        for (int i = 0; i < buttonRequests.Count; i++)
+                        {
+                            List<ButtonPress> result = new List<ButtonPress>();
+                            if (i + 1 > buttonRequests.Count - 1)
+                            {
+                                result = buttonPresses.Where(x => x.time > buttonRequests[i].time).ToList();
+                            }
+                            else
+                            {
+                                result = buttonPresses.Where(x => x.time > buttonRequests[i].time && x.time < buttonRequests[i + 1].time).ToList();
+                            }
+                            writer.WriteLine(String.Format("Requested Button: {0}", buttonRequests[i].buttonName));
+                            foreach (ButtonPress press in result)
+                            {
+                                writer.WriteLine(String.Format("Button Press: {0}   After: {1}ms", press.buttonName, ((int)(press.time-buttonRequests[i].time).TotalMilliseconds)));
+                            }
+                        }
+                    }
+                    
+                    buttonPresses.Clear();
+                    buttonRequests.Clear();
+                }
+                
+                // Get the button presses from the actualButtonPress list between the two button presses from the requestedButton list
+                state = State.Init;
+                button1.Text = "Start";
+            }
+
+        }
+        public enum State
+        {
+            Init = 0,
+            step0 = 1,
+            step1 = 2,
+            step2 = 3,
+            step3 = 4,
+            step4 = 5,
+            step5 = 6,
+            step6 = 7,
+            step7 = 8
+        }
+
+        public enum LogitechF310Buttons
+        {
+            A = 0,
+            B = 1,
+            X = 2,
+            Y = 3,
+            Left_Bumper = 4,
+            Right_Bumper = 5,
+            Back_Button = 6,
+            Start_Button = 7,
+            Left_Stick = 8,
+            Right_Stick = 9
+        }
+
+    }
+}
